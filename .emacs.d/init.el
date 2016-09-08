184617f1209558a8d2012284ee758ba2b1e2c3f5
rosston <ross.brandes@gmail.com>
2016-09-08 11:34:46 -0400

    Use scroll-step rather than scroll-conservatively.
    Appears to keep the same behavior, but scroll-step's intention seems clearer.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -96,8 +96,8 @@
   :init (load-theme 'spacemacs-dark t nil))
 
 (setq column-number-mode t)
-(setq scroll-conservatively 9999
-      scroll-margin 20)
+(setq scroll-margin 20
+      scroll-step 1)
 
 (defun startup-magit ()
   (magit-status)
