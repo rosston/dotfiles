fcf503efacb08d7aa8fdea725ce9de35e26fbb05
Ross Brandes <ross.brandes@gmail.com>
2024-01-12 13:13:39 -0500

    Add keyboard shortcuts for navigating git hunks
    Cribbing from vim-fugitive, and also vim in diff mode.

--- .vim/init.lua
+++ .vim/init.lua
@@ -117,7 +117,32 @@ require("packer").startup(function(use)
 	use({
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					-- Navigation
+					vim.keymap.set("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { buffer = bufnr, expr = true })
+
+					vim.keymap.set("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { buffer = bufnr, expr = true })
+				end,
+			})
 		end,
 		tag = "release",
 	})
